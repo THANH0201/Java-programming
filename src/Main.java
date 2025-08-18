6f1ae05322708a2fdb0c9b49d35d14f54933bc69
THANH0201 <160411131+THANH0201@users.noreply.github.com>
2025-08-18 11:58:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void head(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+    }
+   //print out string inside method main,
+    public static void main(String[] args) {
+        System.out.print("Hello World! It is time to start our world!\n");// print + \n: xuong dong
+        System.out.println("This is the second line!");//println tu xuong dong
+        System.out.println("Hello World!");
+    }
+}

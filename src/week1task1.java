7bbd5d06b9f7df3cbec8dbb1fcbc8bbaa8fb0c22
THANH0201 <160411131+THANH0201@users.noreply.github.com>
2025-08-18 12:31:58 +0300

    cm

---
+++ src/week1task1.java
@@ -0,0 +1,6 @@
+public class week1task1 {
+    public static void main(String[] args ) {
+        System.out.println("Hello! My name is Thanh, ID:2420362");
+        System.out.print("I am in ML class.\nThis is my first exercise about Java programing.");
+    }
+}
